63cc6c008e1f795385919ebc6bd6d1ef161d52b2
Dev Singh <dsingh14@illinois.edu>
2024-06-10 00:03:29 -0400

    bump the version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name='deplyai-cli',
     maintainer="DeplyAI",
     maintainer_email="support@deplyai.com",
-    version='0.3.5',
+    version='0.4.0',
     zip_safe=False,
     description='DeplyAI CLI',
     platforms=['Linux', 'Windows', 'MacOS'],
