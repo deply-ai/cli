688746f1a735ea6fa2d50fc4fa821ffa85a36be8
Dev Singh <dsingh14@illinois.edu>
2024-06-11 20:24:09 -0400

    fix deps

--- src/auth.py
+++ src/auth.py
@@ -4,7 +4,7 @@
 Wrapper to authenticate to the Deply API
 """
 
-import argparse, requests, sys, subprocess
+import argparse, requests, sys, subprocess, os
 from .util import delete_profile_credentials, set_profile_credentials
 import time
 USAGE_HEAD: str = '''\
@@ -81,7 +81,8 @@ class AuthHandler(object):
             formatter_class=argparse.RawDescriptionHelpFormatter
         )
         self.parser.add_argument('verb', type=str, help='The action to take', choices=['login', 'logout'])
-        self.parser.add_argument('--profile', type=str, help='The profile to store credentials for', dest='profile', default='default')
+        self.parser.add_argument('--profile', type=str, help='The profile to store credentials for', dest='profile', default=os.environ.get("DEPLY_PROFILE", "default"))
+        self.parser.add_argument('--token', type=str, help='Organization Access Token value (if authenticating silently)', dest='token')
     def run(self, unparsed_args: list):
         """
         Parse and execute the command
@@ -93,45 +94,59 @@ class AuthHandler(object):
         match args.verb:
             case 'login':
                 print(f"Authenticating user to profile \"{profile}\"...")
-                data = post_device_code()
-                device_code = data['device_code']
-                verification_uri_complete = data['verification_uri_complete']
-                polling_interval = data['interval']
-                print(
-f"""
-Attempting to automatically open the authentication page in your default browser.
-If the browser does not open, open the following URL:
-
-{verification_uri_complete}
-
-and complete the authentication steps.
-"""
-                )
-                try:
-                    if sys.platform=='win32':
-                        os.startfile(verification_uri_complete)
-                    elif sys.platform=='darwin':
-                        subprocess.Popen(['open', verification_uri_complete])
-                    else:
-                        subprocess.Popen(['xdg-open', verification_uri_complete])
-                except:
-                    pass
-                token_response = poll_token_endpoint(device_code, timeout=polling_interval)
-                if 'access_token' in token_response and 'id_token' in token_response:
+                if args.token != None:
                     try:
-                        print("Finishing up...")
-                        discovery_data = discover_tenant(token_response['access_token'])
+                        print("Authenticating using provided token...")
+                        discovery_data = discover_tenant(args.token)
                         tenant = int(discovery_data['tenant_id'])
                         set_profile_credentials(profile=profile, credentials={
-                            "token": token_response['access_token'], 
-                            "id_token": token_response['id_token'], 
+                            "token": args.token, 
+                            "id_token": args.token, 
                             "tenant": tenant
                         })
                         print("Authentication complete!")
                     except:
                         print("Recieved tokens but could not complete authentication process. Please try again.")
                 else:
-                    print('Failed to retrieve access token:', token_response.get('message'))
+                    data = post_device_code()
+                    device_code = data['device_code']
+                    verification_uri_complete = data['verification_uri_complete']
+                    polling_interval = data['interval']
+                    print(
+    f"""
+    Attempting to automatically open the authentication page in your default browser.
+    If the browser does not open, open the following URL:
+
+    {verification_uri_complete}
+
+    and complete the authentication steps.
+    """
+                    )
+                    try:
+                        if sys.platform=='win32':
+                            os.startfile(verification_uri_complete)
+                        elif sys.platform=='darwin':
+                            subprocess.Popen(['open', verification_uri_complete])
+                        else:
+                            subprocess.Popen(['xdg-open', verification_uri_complete])
+                    except:
+                        pass
+                    token_response = poll_token_endpoint(device_code, timeout=polling_interval)
+                    if 'access_token' in token_response and 'id_token' in token_response:
+                        try:
+                            print("Finishing up...")
+                            discovery_data = discover_tenant(token_response['access_token'])
+                            tenant = int(discovery_data['tenant_id'])
+                            set_profile_credentials(profile=profile, credentials={
+                                "token": token_response['access_token'], 
+                                "id_token": token_response['id_token'], 
+                                "tenant": tenant
+                            })
+                            print("Authentication complete!")
+                        except:
+                            print("Recieved tokens but could not complete authentication process. Please try again.")
+                    else:
+                        print('Failed to retrieve access token:', token_response.get('message'))
             case 'logout':
                 print(f"Logging out of profile \"{profile}\"...")
                 delete_profile_credentials(profile=profile)
